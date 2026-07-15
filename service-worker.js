679780dc1485d8b2d65d4cb73e179f7e686d16c5
Averniq <gary.zhang2023@gmail.com>
2026-07-15 19:52:25 +1000

    Fill mobile menu photo rails

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = "sake-street-shell-v5";
-const RUNTIME_CACHE = "sake-street-runtime-v5";
+const SHELL_CACHE = "sake-street-shell-v6";
+const RUNTIME_CACHE = "sake-street-runtime-v6";
 const APP_SHELL = [
   "/",
   "/index.html",
