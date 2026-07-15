f347acb4e91f2e8ceb68eb4a4c5fbdad4ed14f14
Averniq <gary.zhang2023@gmail.com>
2026-07-15 21:55:20 +1000

    Make Sake Street header logo prominent

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = "sake-street-shell-v8";
-const RUNTIME_CACHE = "sake-street-runtime-v8";
+const SHELL_CACHE = "sake-street-shell-v9";
+const RUNTIME_CACHE = "sake-street-runtime-v9";
 const APP_SHELL = [
   "/",
   "/index.html",
